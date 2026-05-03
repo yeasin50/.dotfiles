4bef4a602016132e149dd8950be1c2292905e390
yeasin50 <46500228+yeasin50@users.noreply.github.com>
2026-05-04 03:22:00 +0600

    removed(rofi) kvm while using QUME as default

--- rofi/.config/rofi/scripts/menu.sh
+++ rofi/.config/rofi/scripts/menu.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
 
-# Format: "Icon Name: Description"
-options="📝 note: Neovim Notes
-📋 kaban: Obsidian Kanban
-🖥️  desktopMode: System Layout
-🎥 recordMode: Screen Record
-📦 vboxmode: VirtualBox
-🚀 ventoryQemu: QEMU/KVM os path"
+choice=$(printf "🧭 Apps\n📝 note\n📋 kaban\n🖥️ desktopMode\n🎥 recordMode" | rofi -dmenu -i -p "Menu")
 
-choice_row=$(echo -e "$options" | rofi -dmenu -i -p "Menu" -lines 10)
-
-choice=$(echo "$choice_row" | awk '{print $2}' | tr -d ':')
-
-[[ -z "$choice" ]] && exit 0
+[ -z "$choice" ] && exit 0
 
 case "$choice" in
-    "note")        ~/.config/rofi/scripts/rofi_note.sh ;;
-    "kaban")       ~/.config/rofi/scripts/kaban.sh ;;
-    "desktopMode") bash -ic desktopMode ;;
-    "recordMode")  bash -ic recordMode ;;
-    "vboxmode")    bash -ic vboxmode ;;
-    "ventoryQemu")     bash -ic ventoryQemu ;;
+    *Apps*)
+        rofi -show drun
+        ;;
+    *note*)
+        ~/.config/rofi/scripts/rofi_note.sh
+        ;;
+    *kaban*)
+        ~/.config/rofi/scripts/kaban.sh
+        ;;
+    *desktopMode*)
+        bash -ic desktopMode
+        ;;
+    *recordMode*)
+        bash -ic recordMode
+        ;;
 esac
